6fa1a7a5b656a53267351c4848d9ece94be635ef
Maks <mavol_55@mail.ru>
2023-05-22 20:29:16 +0300

    add react-hook-form

---
+++ dist/client/main.63d3cb468dc04c888db3.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/shared/CardsList/FormComments/index.ts":
+/*!****************************************************!*\
+  !*** ./src/shared/CardsList/FormComments/index.ts ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    var desc = Object.getOwnPropertyDescriptor(m, k);\r\n    if (!desc || (\"get\" in desc ? !m.__esModule : desc.writable || desc.configurable)) {\r\n      desc = { enumerable: true, get: function() { return m[k]; } };\r\n    }\r\n    Object.defineProperty(o, k2, desc);\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __exportStar = (this && this.__exportStar) || function(m, exports) {\r\n    for (var p in m) if (p !== \"default\" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\n__exportStar(__webpack_require__(/*! ./FormComments */ \"./src/shared/CardsList/FormComments/FormComments.tsx\"), exports);\r\n\n\n//# sourceURL=webpack:///./src/shared/CardsList/FormComments/index.ts?");
+
+/***/ })
+
+})
